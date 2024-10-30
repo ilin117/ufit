708e23e13644b7d34ed7ba4e1724615845f6fa7e
Issac Lin <ilin1172004@gmail.com>
2024-10-30 15:49:48 -0400

    finished navbar

---
+++ uFit/static/js/navbar.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("nav-sidebar").style.width = "200px";
+}
+
+function closeNav() {
+    document.getElementById("nav-sidebar").style.width = "0";
+}
+
+function toggleMenu() {
+    document.getElementById("profileOptions").classList.toggle("open-options");
+}
