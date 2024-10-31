7da6d8c94e7f18b11190f6ab5dc0dd7458f27035
[ilin117] <[ilin1172004@gmail.com]>
2024-10-31 14:24:32 -0400

    ready for merge issac

--- uFit/uFit/urls.py
+++ uFit/uFit/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+from .views import privacyPage
 
 urlpatterns = [
     path("admin/", admin.site.urls),
