7cce76499950f5183df91f3375982c7bc2b74762
Samuel Santos <157077824+Sam-Is-Red@users.noreply.github.com>
2024-11-07 16:32:46 -0500

    Made footer

--- uFit/base/urls.py
+++ uFit/base/urls.py
@@ -2,3 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [path("", views.say_hello, name="hello")]
+
+urlpatterns = [path("", views.home, name="home")]
+urlpatterns = [path("", views.chatpage, name="chatpage")]
--- uFit/base/views.py
+++ uFit/base/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def say_hello(request):
     return render(request, "base/index.html", {})
+
+def home(request):
+    return render(request, 'home.html')
+
+def chatpage(request):
+    return render(request, 'chatpage.html')
