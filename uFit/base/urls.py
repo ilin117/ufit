611763b459ac61933387106c69eebd8ebd3cadd8
rashidmoreau <rashidmoreau@gmail.com>
2024-11-14 14:14:29 -0500

    Added home, login, and registration pages

--- uFit/base/urls.py
+++ uFit/base/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 
 urlpatterns = [path("", views.say_hello, name="hello"),
-               path("privacy/", views.privacyPage, name="privacy"),]
+               path("privacy/", views.privacyPage, name="privacy"),
+               path("home/", views.homePage, name="home"),]
