5bbf40a06c7b056dcf79d3a1caa2e3afd2a5de41
[ilin117] <[ilin1172004@gmail.com]>
2024-12-01 19:39:41 -0500

    populate db

--- uFit/base/models.py
+++ uFit/base/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    host = models.CharField(max_length=200)
+
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def str(self):
+        return self.title
+
+
+class User(models.Model):
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
