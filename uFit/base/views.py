0fee63bb6e7e331421925a0292cda31e43ef501f
Destroyer314 <matthewsherin17@gmail.com>
2024-12-05 15:44:29 -0500

    Search?

--- uFit/base/views.py
+++ uFit/base/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
+from django.db.models import Q
+from .models import Post
 
 # Create your views here.
 def welcomePage(request):
@@ -19,7 +21,14 @@ def privacyPage(request):
 
 @login_required
 def homePage(request):
-    return render(request, "base/home.html")
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    posts = Post.objects.filter(
+        Q(title__icontains=q) | 
+        Q(content__icontains=q) | 
+        Q(author__username__icontains=q)
+    )
+    context = {"posts": posts}
+    return render(request, "base/home.html", context)
 
 
 def registration_page(request):
