0839a6c4f9161ee650f2dfce6f1a4455e6cf2b60
Rashid Moreau <114450015+rashidmoreau@users.noreply.github.com>
2024-12-07 22:40:02 -0500

    Made Migrations
    Deleted and reapplied migrations

--- uFit/base/models.py
+++ uFit/base/models.py
@@ -5,30 +5,21 @@ from django.contrib.auth.models import User
 # from django.contrib.auth.models import AbstractUser
 # from django.utils.timezone import now
 # Create your models here.
-class Post(models.Model):
-    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    ##topic = modles.ForeignKey(Topic, on_delete = models.SET_NULL, null = True)
-    description = models.TextField(null=True, blank=True)
-
-    updated = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        ordering = ["-update", "-created"]
-
-    def str(self):
-        return self.title
-
 
 # Create your models here.
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
     body = models.TextField()
-    host = models.CharField(max_length=200)
-
-    updated = models.DateTimeField(auto_now=True)
+    host = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.title
 
     def str(self):
         return self.title
